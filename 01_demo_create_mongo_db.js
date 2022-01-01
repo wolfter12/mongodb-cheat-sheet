1d8b9efb4b328c5551ab6f8d8f367e6c37d15feb
Nazar <wolfter12@gmail.com>
2022-01-02 00:42:13 +0200

    add demo_create_mongo_db.js

---
+++ 01_demo_create_mongo_db.js
@@ -0,0 +1,11 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, (err, db) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Database created!");
+
+  db.close();
+});
